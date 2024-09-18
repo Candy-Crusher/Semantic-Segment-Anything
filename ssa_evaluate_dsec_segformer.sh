e5b32626a6a8e5c70dadb1ecfd1d6f029025d34f
Candy-Crusher <cate.xiaoshan.wu@gmail.com>
2024-09-18 09:58:13 +0000

    add evaluate segformer output

--- ssa_evaluate_dsec_segformer.sh
+++ ssa_evaluate_dsec_segformer.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=2,3
 scene='night'
 # 运行Python脚本
 
-python scripts/evaluation.py \
+python scripts/evaluation_segformer.py \
     --scene ${scene} \
     --gt_path /home/xiaoshan/work/adap_v/my_proj/data/DSEC/${scene}/gtFine/val/ \
     --result_path dsec_${scene}_val_19/ \
